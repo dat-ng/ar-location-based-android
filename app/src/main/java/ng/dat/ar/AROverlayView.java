f7777bb08ec6794e69b2300c38361aab4c4be6e0
Dat Nguyen Thanh <dat.nguyen@axonactive.vn>
2017-01-16 14:45:42 +0700

    no message

--- app/src/main/java/ng/dat/ar/AROverlayView.java
+++ app/src/main/java/ng/dat/ar/AROverlayView.java
@@ -34,8 +34,8 @@ public class AROverlayView extends View {
 
         //Demo points
         arPoints = new ArrayList<ARPoint>() {{
-            add(new ARPoint("Sun Wheel", 16.0404856, 108.2262447, 10));
-            add(new ARPoint("Linh Ung Pagoda", 16.1072989, 108.2343984, 200));
+            add(new ARPoint("Sun Wheel", 16.0404856, 108.2262447, 0));
+            add(new ARPoint("Linh Ung Pagoda", 16.1072989, 108.2343984, 0));
         }};
     }
 
